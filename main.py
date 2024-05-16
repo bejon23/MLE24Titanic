5a7be6c62a000032fcd6f80bfbebb8ae653fc5ff
ronysaimon16 <167416399+ronysaimon16@users.noreply.github.com>
2024-05-16 16:16:24 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
-from sklearn.preprocessing import LabelEncoder
 import pickle
 import os
 
@@ -16,12 +15,6 @@ templates_dir = os.path.join(os.path.dirname(__file__), "templates")
 static_dir = os.path.join(os.path.dirname(__file__), "static")
 templates = Jinja2Templates(directory=templates_dir)
 
-# Label encoder for 'Sex'
-sex_encoder = LabelEncoder()
-
-# Label encoder for 'Embarked'
-embarked_encoder = LabelEncoder()
-
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
@@ -29,21 +22,15 @@ async def read_item(request: Request):
 @app.post("/predict/")
 async def predict(request: Request,
                   pclass: int = Form(...),
-                  sex: str = Form(...),
+                  sex: int = Form(...),  # Change type to int
                   age: int = Form(...),
                   sibsp: int = Form(...),
                   parch: int = Form(...),
                   fare: int = Form(...),
-                  embarked: str = Form(...),
+                  embarked: int = Form(...),  # Change type to int
                   familysize: int = Form(...)):
 
-    # Convert 'Sex' to numerical value using label encoding
-    sex_encoded = sex_encoder.transform([sex])
-
-   # Convert 'Embarked' to numerical value using label encoding
-    embarked_encoded = embarked_encoder.transform([embarked])
-
-    features = [pclass, sex_encoded[0], age, sibsp, parch, fare, embarked, familysize]
+    features = [pclass, sex, age, sibsp, parch, fare, embarked, familysize]
 
     # Make prediction
     prediction = model.predict([features])[0]
